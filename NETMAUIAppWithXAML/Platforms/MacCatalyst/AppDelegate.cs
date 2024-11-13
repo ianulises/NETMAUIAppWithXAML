0d5889f764920ab609c7a8ec9f0fd77373da33f4
ulises <ulises.sandria@brendcar.com>
2024-11-13 13:40:46 -0600

    Agregar archivos de proyecto.

---
+++ NETMAUIAppWithXAML/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace NETMAUIAppWithXAML
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
